12374c56bea266d7b5e794134c8cb6c9ab8940af
C0MaE <jannis.koberg@icloud.com>
2025-07-26 21:21:44 +0200

    added publishing maven and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    `maven-publish`
 }
 
 group = "dev.comae"
@@ -20,4 +21,34 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = "dev.comae"
+            artifactId = "McLookup"
+            version = version
+
+            pom {
+                name.set("McLookup")
+                description.set("A very simple Minecraft Lookup library")
+                url.set("https://github.com/C0MaE/McLookup")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OwnServer"
+            url = uri("https://repo.comae.dev/repository/maven-releases/") // oder snapshot repo
+
+            credentials {
+                username = findProperty("mavenUser") as String? ?: System.getenv("MAVEN_USERNAME")
+                password = findProperty("mavenPassword") as String? ?: System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
